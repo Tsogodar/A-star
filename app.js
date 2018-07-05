a966052044be583f63e9f22f7f0f95a56a240385
Marcin Gryczon <marcin.gryczon@cormo.pl>
2018-07-05 21:35:33 +0200

    przeliczanie metryką euklidesową dostępnych pól do odwiedzenia

--- app.js
+++ app.js
@@ -4,55 +4,46 @@ const loadArrayFromFile = (filename) => {
     let array = [];
     let file = fs.readFileSync(`../${filename}`, 'utf8').split('\r\n');
     file.forEach((row) => {
-        array.push(row.split(' ').map(Number))
+        array.push(row.split(' ').map(Number));
     });
     return array;
 };
 
 //metryka euqlidesa
 const euclidHeuristic = (point, destination) => {
-    return Math.sqrt(Math.pow((point[0] - destination[0]), 2) + Math.pow((point[1] - destination[1]), 2))
+    return Math.sqrt(Math.pow((point[0] - destination[0]), 2) + Math.pow((point[1] - destination[1]), 2));
 };
 
 //mechanizm odwiedzania pól
 const visit = (parent, map) => {
-    let canVisit=[{'parent': [parent[0], parent[1]]}];
+    let canVisit = [{'parent': [parent[0], parent[1]]}];
     let candidate = [
         {'bottom': [parent[0], parent[1] + 1]},
         {'left': [parent[0] - 1, parent[1]]},
         {'top': [parent[0], parent[1] - 1]},
         {'right': [parent[0] + 1, parent[1]]}
     ];
-    candidate.forEach((direction, index) => {
-        let cords=direction[Object.keys(direction)]
-        let value =map[cords[0]][cords[1]]
-        if(value!==5){
+    candidate.forEach((direction) => {
+        let cords = direction[Object.keys(direction)];
+        let value = map[cords[0]][cords[1]];
+        if (value !== 5) {
             canVisit.push(cords)
         }
-    })
+    });
     return canVisit;
 };
 
 //główny program wykonywalny
 const main = (start, end) => {
-    const map = loadArrayFromFile('pdf.txt')
+    const map = loadArrayFromFile('pdf.txt');
     let closeList = [start];
     let openList = [];
-    console.log(loadArrayFromFile('pdf.txt'));
-    // console.log(euclidHeuristic(start[0], start[1], end[0], end[1]));
-    // console.log(closeList);
-    // console.log(visit(start))
-    openList.push(visit(start, map))
-    console.log(openList);
-    //todo ostatnio robione - liczymy metrykę euklidesa dla wszystkich elementów w liście otwartej
+    openList.push(visit(start, map));
     openList.forEach(item => {
-        console.log(item)
-        item.forEach(positions => {
-            // if (Object.keys(positions)[0] !== 'parent') {
-            //     // let position = positions[Object.keys(positions)]
-            //     // console.log(position)
-            //     // console.log(euclidHeuristic(position, end))
-            // }
+        item.forEach(position => {
+            if (Object.keys(position)[0] !== 'parent') {
+                    console.log(euclidHeuristic(position, end))
+            }
         })
     })
 
