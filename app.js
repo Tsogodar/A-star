34340a7153d8271813b009a2656346604c51be10
Marcin Gryczon <marcin.gryczon@cormo.pl>
2018-07-20 18:42:03 +0200

    OOP

--- app.js
+++ app.js
@@ -1,94 +1,53 @@
-//ładowanie tablicy z pliku
-const loadArrayFromFile = (filename) => {
-    const fs = require('fs');
-    let array = [];
-    let file = fs.readFileSync(`../${filename}`, 'utf8').split('\r\n');
-    file.forEach((row) => {
-        array.push(row.split(' ').map(Number));
-    });
-    return array;
-};
+class Astar {
+	constructor(filename) {
+		const fs = require('fs')
+		let array = []
+		let file = fs.readFileSync(`../${filename}`, 'utf8').split('\r\n')
+		file.forEach((row) => {
+			array.push(row.split(' ').map(Number))
+		})
+		this.map = array
+	}
 
-//metryka euqlidesa
-const euclidHeuristic = (point, destination) => {
-    return Math.sqrt(Math.pow((point[0] - destination[0]), 2) + Math.pow((point[1] - destination[1]), 2));
-};
+	euclidHeuristic(point, destination) {
+		return Math.sqrt((point[0] - destination[0]) ** 2 + (point[1] - destination[1]) ** 2)
+	}
 
-//mechanizm odwiedzania pól
-const visit = (parent, map) => {
-    let canVisit = [{'parent': [parent[0], parent[1]]}];
-    let candidate = [
-        {'bottom': [parent[0], parent[1] + 1]},
-        {'left': [parent[0] - 1, parent[1]]},
-        {'top': [parent[0], parent[1] - 1]},
-        {'right': [parent[0] + 1, parent[1]]}
-    ];
-    console.log(candidate)
-    candidate.forEach((direction) => {
-        let cords = direction[Object.keys(direction)];
-        if ((cords[0] >= 0 || cords[1] >= 0) && (cords[0] >= 0 && cords[1] >= 0)) {
-            let value = map[cords[0]][cords[1]];
-            console.log(value)
-            if (value !== 5) {
-                canVisit.push(cords)
-            }
-        }
-    });
-    return canVisit;
-};
-//todo poprawić warunek w metodzie visit
-const findPath = (openList, closeList, start, end, map, euclidesValues) => {
-    let openListLength = openList.length
-    openList.push(visit(start, map));
-    openList[openListLength].forEach(position => {
-        if (Object.keys(position)[0] !== 'parent') {
-            euclidesValues.push([position, euclidHeuristic(position, end)]);
-        }
-    })
-// console.log(euclidesValues);
-    minimalCost = euclidesValues[0][1];
-    euclidesValues.forEach(value => {
-        if (value[1] > minimalCost) {
-            minimalCost = value;
-            closeList.push(value[0])
-            start = value[0]
-        }
-    })
-    // console.log(closeList)
-    // console.log(openList)
-    return findPath(openList, closeList, start, end, map, euclidesValues)
+	visit(parent, map) {
+		let canVisit = [{
+			'parent': [parent[0], parent[1]]
+		}]
+		let candidate = [{
+			'bottom': [parent[0], parent[1] + 1]
+		},
+		{
+			'left': [parent[0] - 1, parent[1]]
+		},
+		{
+			'top': [parent[0], parent[1] - 1]
+		},
+		{
+			'right': [parent[0] + 1, parent[1]]
+		}
+		]
+		candidate.forEach((direction) => {
+			let cords = direction[Object.keys(direction)]
+			if (cords[0] >= 0 || cords[1] >= 0) {
+				let value = map[cords[0]][cords[1]]
+				console.log(value)
+				if (value !== 5) {
+					canVisit.push(cords)
+				}
+			}
+		})
+		return canVisit
+	}
+
 }
 
-//główny program wykonywalny
-const main = (start, end) => {
-    let minimalCost = 0;
-    let euclidesValues = [];
-    const map = loadArrayFromFile('pdf.txt');
-    let closeList = [];
-    let openList = [];
-    closeList.push(start)
-    findPath(openList, closeList, start, end, map, euclidesValues)
-    // openList.push(visit(start, map));
-    // openList.forEach(item => {
-    //     item.forEach(position => {
-    //         if (Object.keys(position)[0] !== 'parent') {
-    //             euclidesValues.push([position, euclidHeuristic(position, end)]);
-    //         }
-    //     })
-    // });
-    // // console.log(euclidesValues);
-    // minimalCost =euclidesValues[0][1];
-    //     euclidesValues.forEach(value => {
-    //         if(value[1]>minimalCost){
-    //             minimalCost=value;
-    //             closeList.push(value[0])
-    //         }
-    //     })
-    // console.log(closeList)
-
-};
-
-
-console.time('execution time');
-main([1, 2], [5, 1]);
-console.timeEnd('execution time');
+console.time('execution time')
+const trace = new Astar('pdf.txt')
+console.log(trace.map)
+trace.visit([1, 2], trace.map)
+trace.euclidHeuristic([1, 2], [5, 1])
+console.timeEnd('execution time')
